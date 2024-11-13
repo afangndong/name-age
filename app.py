1cdcafff6734cbcd1df4519ec234ca356f43b508
afangndong <43615715+afangndong@users.noreply.github.com>
2024-11-13 13:10:03 +0100

    Create app.py
    first commit of our application file .py

---
+++ app.py
@@ -0,0 +1,8 @@
+def main():
+  print("Hello!!!")
+  name = input("What is your name? ")
+  age = input("How old are you? ")
+  print(f"{name} is {age} years old.")
+
+if __name__ == "__main__":
+  main()
